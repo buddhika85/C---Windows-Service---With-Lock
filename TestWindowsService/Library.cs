0501091a6fa735ad52ee7860a6da7e0498fb1827
buddhika85 <buddhika2006system@gmail.com>
2018-07-22 12:01:00 +0530

    initial commit

---
+++ TestWindowsService/Library.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestWindowsService
+{
+    public static class Library
+    {
+        static readonly object _object = new object();
+        private static Thread thread;
+        public static void WriteErrorLog(Exception ex)
+        {
+            try
+            {
+                var sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now + " - Exception : " + ex.Source.Trim() + ": " + ex.Message.Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+
+        public static void WriteLog(string message)
+        {
+            try
+            {
+                var sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now + ": " + message);
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                WriteErrorLog(e);
+            }
+        }
+
+
+      
+
+        private static void DoThreadJob()
+        {
+            try
+            {
+                lock (_object)
+                {
+                    WriteLog(Thread.CurrentThread.Name + " - STARTS");
+                    for (var i = 1; i < 20; i++)
+                    {
+                        Thread.Sleep(1000);
+                        WriteLog(Thread.CurrentThread.Name + " - " + i);
+                    }
+                    WriteLog(Thread.CurrentThread.Name + " - COMPLETES");
+                    thread = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLog("EXCEPTION in DoThreadJob");
+                WriteErrorLog(ex);
+            }
+        }
+
+        public static void ManageLongRunningTask()
+        {
+            try
+            {
+                //var lockedBySomeoneElse = !Monitor.TryEnter(_object);
+                if (thread == null && !Monitor.IsEntered(_object)) //if (thread == null && !lockedBySomeoneElse) 
+                {
+                    thread = new Thread(DoThreadJob) { IsBackground = false, Name = Guid.NewGuid().ToString() };
+                    thread.Start();
+                }
+                //DoThreadJob();
+
+
+            }
+            catch (Exception e)
+            {
+                WriteLog("EXCEPTION in ManageLongRunningTask");
+                WriteErrorLog(e);
+            }
+        }
+    }
+}
---
+++ TestWindowsService/Scheduler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace TestWindowsService
+{
+    // https://www.c-sharpcorner.com/UploadFile/naresh.avari/develop-and-install-a-windows-service-in-C-Sharp/?
+    // Developer command prompt for visual studio
+    // Set path to debug forlder - cd D:\Study projects\TestWindowsService\TestWindowsService\bin\Debug
+    // InstallUtil.exe "TestWindowsService.exe"
+    // InstallUtil.exe /u "TestWindowsService.exe"
+    public partial class Scheduler : ServiceBase
+    {
+        private Timer _timer1;
+        
+
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                _timer1 = new Timer { Interval = 5000 };    // every 5 seconds
+
+                _timer1.Elapsed += Timer1_Tick;
+                _timer1.Enabled = true;
+                Thread.CurrentThread.Name = "Main Thread";
+                Library.WriteLog("Test Windows Service Started");
+            }
+            catch (Exception e)
+            {
+                Library.WriteLog("Error on starting windows service, excpetion details will follow - ");
+                Library.WriteErrorLog(e);
+            }
+        }
+
+        private void Timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                Library.WriteLog("Timer Ticked");
+                Library.ManageLongRunningTask();
+            }
+            catch (Exception exception)
+            {
+                Library.WriteLog("Error on ticking windows service, excpetion details will follow - ");
+                Library.WriteErrorLog(exception);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                _timer1.Enabled = false;
+                Library.WriteLog("Test Windows Service Stopped");
+            }
+            catch (Exception e)
+            {
+                Library.WriteLog("Error on stopping windows service, excpetion details will follow - ");
+                Library.WriteErrorLog(e);
+            }
+        }
+    }
+}
